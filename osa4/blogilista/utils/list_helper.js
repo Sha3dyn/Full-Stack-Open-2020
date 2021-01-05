64d198b692ecb7e9346e853ec91ecad928be9fac
Mirka Poikelin <mirka.poikelin@gmail.com>
2021-01-05 17:29:46 +0200

    Exercise 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -5,11 +5,20 @@ const dummy = (blogs) => {
   
 const totalLikes = (blogs) => {
     var result = blogs.map(blog => blog.likes).reduce((acc, blog) => blog + acc, 0)
-    console.log(result)
     return result
+}
+
+const favoriteBlog = (blogs) => {
+    blogsCopy = [...blogs]
+    
+    var sorted = blogsCopy.sort((a, b) => b.likes - a.likes)
+    var result = blogs.length > 0 ? sorted[0] : 0
+
+    return result;
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
